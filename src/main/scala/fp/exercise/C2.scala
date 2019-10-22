902dd6715dbbb7b2450e2eb3cb1e366c7047b849
nooberfsh <nooberfsh@gmail.com>
2019-10-22 11:59:29 +0800

    Impl c2 exercises

---
+++ src/main/scala/fp/exercise/C2.scala
@@ -0,0 +1,39 @@
+package fp.exercise
+
+import scala.annotation.tailrec
+
+object C2 {
+  // c2.1
+  def fib(n: Int): Int = {
+    @tailrec
+    def go(n: Int, l: Int, r: Int): (Int, Int) = {
+      if (n == 0) (l, r)
+      else go(n - 1, r, l + r)
+    }
+    go(n, 0, 1)._1
+  }
+
+  // c2.2
+  @tailrec
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean =
+    as match {
+      case Array() | Array(_) => true
+      case _ =>
+        val head = as(0)
+        // TODO: eliminate alloc
+        val rest = as.slice(1, as.length)
+        if (ordered(head, rest(0))) isSorted(rest, ordered)
+        else false
+    }
+
+  // c2.3
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => f(a, _)
+
+  // c2.4
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
+  // c2.5
+  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
+
+  def andThen[A, B, C](f: A => B, g: B => C): A => C = a => g(f(a))
+}
---
+++ src/test/scala/fp/exercise/C2Test.scala
@@ -0,0 +1,51 @@
+package fp.exercise
+
+import org.scalatest.{DiagrammedAssertions, FunSuite}
+import C2._
+
+class C2Test extends FunSuite with DiagrammedAssertions {
+  test("fib") {
+    assert(fib(0) === 0)
+    assert(fib(1) === 1)
+    assert(fib(2) === 1)
+    assert(fib(3) === 2)
+    assert(fib(4) === 3)
+    assert(fib(5) === 5)
+    assert(fib(6) === 8)
+  }
+
+  test("isSorted") {
+    val compare = (a: Int, b: Int) => a < b
+    assert(isSorted(Array(), compare) === true)
+    assert(isSorted(Array(1), compare) === true)
+
+    assert(isSorted(Array(1, 2), compare) === true)
+    assert(isSorted(Array(2, 1), compare) === false)
+
+    assert(isSorted(Array(1, 2, 2), compare) === true)
+    assert(isSorted(Array(1, 2, 7, 6), compare) === false)
+  }
+
+  test("curry") {
+    val f = (a: Int, b: Int) => a + b
+    val curried = curry(f)
+    assert(curried(1)(2) === 3)
+  }
+
+  test("uncurry") {
+    val f = (a: Int) => (b: Int) => a + b
+    val uncurried = uncurry(f)
+    assert(uncurried(1, 2) === 3)
+  }
+
+  test("compose") {
+    val addOne = (a: Int) => a + 1
+    val multiTwo = (b: Int) => b * 2
+
+    val composed1 = compose(addOne, multiTwo)
+    assert(composed1(10) === 21)
+
+    val composed2 = compose(multiTwo, addOne)
+    assert(composed2(10) === 22)
+  }
+}
