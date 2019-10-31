b15991afdf7150669c7bdb2faff1495d6952463a
nooberfsh <nooberfsh@gmail.com>
2019-10-31 17:57:21 +0800

    Add some C5 test

--- src/main/scala/fp/exercise/C5.scala
+++ src/main/scala/fp/exercise/C5.scala
@@ -40,7 +40,7 @@ object C5 {
       }
 
     def tail: Stream[A] = this match {
-      case Cons(h, t) => t()
+      case Cons(_, t) => t()
       case _          => Stream.empty
     }
 
@@ -76,6 +76,94 @@ object C5 {
     def flatMap[B >: A](f: A => Stream[B]): Stream[B] =
       foldRight(Stream.empty[B])((a, b) => f(a).append(b))
 
+    // c5.13
+    def _map[B](f: A => B): Stream[B] =
+      Stream.unfold(this) {
+        case Cons(h, t) => Some(f(h()), t())
+        case _          => None
+      }
+    def _take(n: Int): Stream[A] =
+      Stream.unfold((n, this)) {
+        case (k, Cons(h, t)) if k > 0 => Some(h(), (n - 1, t()))
+        case _                        => None
+      }
+    def __takeWhile(p: A => Boolean): Stream[A] =
+      Stream.unfold(this) {
+        case Cons(h, t) if p(h()) => Some(h(), t())
+        case _                    => None
+      }
+    def zipWith[B](s: Stream[B]): Stream[(A, B)] =
+      Stream.unfold((this, s)) {
+        case (Cons(h1, t1), Cons(h2, t2)) => Some((h1(), h2()), (t1(), t2()))
+        case _                            => None
+      }
+    def zipAll[B](s: Stream[B]): Stream[(Option[A], Option[B])] =
+      Stream.unfold((this, s)) {
+        case (Cons(h1, t1), Cons(h2, t2)) =>
+          Some((Some(h1()), Some(h2())), (t1(), t2()))
+        case (Cons(h1, t1), Empty) => Some((Some(h1()), None), (t1(), Empty))
+        case (Empty, Cons(h2, t2)) => Some((None, Some(h2())), (Empty, t2()))
+        case _                     => None
+      }
+
+    def exists(p: A => Boolean): Boolean =
+      this.foldRight(false)((a, b) => p(a) || b)
+
+    def hasSubsequence[B >: A](sub: Stream[B]): Boolean = {
+      Stream
+        .unfold(this) {
+          case s @ Cons(_, t) => Some(s.zipAll(sub), t())
+          case _              => None
+        }
+        .appendOne(Stream.empty.zipAll(sub))
+        .exists(_.forAll { case (a, b) => a == b })
+    }
+
+    // c5.14
+    def startsWith[B >: A](s: Stream[B]): Boolean = (this, s) match {
+      case (Cons(h1, t1), Cons(h2, t2)) =>
+        (h1() == h2()) && t1().startsWith(t2())
+      case (Empty, Cons(_, _)) => false
+      case (Cons(_, _), Empty) => true
+      case (Empty, Empty)      => true
+    }
+
+    def _startsWith[B >: A](s: Stream[B]): Boolean =
+      zipAll(s).takeWhile(_._2.isDefined).forAll { case (a, b) => a == b }
+
+    // c5.15
+    def tails: Stream[Stream[A]] = {
+      Stream.unfold(this) {
+        case s @ Cons(_, t) => Some(s, t())
+        case _              => None
+      } appendOne Stream.empty
+    }
+
+    // c5.16
+    def scanRight[B](z: B)(f: (A, B) => B): Stream[B] = {
+      val f2 = (a: A, b: B, s: Stream[B]) => {
+        val c = f(a, b)
+        (c, Stream.cons(c, s))
+      }
+
+      def go(z: B, s: Stream[A])(
+          f: (A, B, Stream[B]) => (B, Stream[B])): (B, Stream[B]) = s match {
+        case Cons(h, t) =>
+          val (b, sb) = go(z, t())(f)
+          f(h(), b, sb)
+        case Empty => (z, Stream.apply(z))
+      }
+
+      go(z, this)(f2)._2
+    }
+
+    def _scanRight[B](z: B)(f: (A, B) => B): Stream[B] =
+      foldRight((z, Stream(z))) {
+        case (a, (b, s)) =>
+          val c = f(a, b)
+          (c, Stream.cons(c, s))
+      }._2
+
   }
 
   case object Empty extends Stream[Nothing]
@@ -99,7 +187,7 @@ object C5 {
     // c5.8
     def constant[A](a: A): Stream[A] = cons(a, constant(a))
 
-    // c5.8
+    // c5.9
     def from(n: Int): Stream[Int] = cons(n, from(n + 1))
 
     def zip[A, B, C](l: Stream[A], r: Stream[B])(f: (A, B) => C): Stream[C] =
@@ -109,7 +197,26 @@ object C5 {
         case _ => empty[C]
       }
 
-    // c5.9
+    // c5.10
     def fibs: Stream[Int] = cons(0, cons(1, zip(fibs.tail, fibs)(_ + _)))
+
+    //////////////////////////////////////////////////////////////////////////////
+
+    // c5.11
+    def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+      f(z) match {
+        case Some((a, s)) => cons(a, unfold(s)(f))
+        case None         => empty
+      }
+
+    // c5.12
+    def _fibs: Stream[Int] = unfold((0, 1)) {
+      case (a, b) =>
+        Some((a, (b, a + b)))
+    }
+    def _from(n: Int): Stream[Int] = unfold(n)(n => Some(n, n + 1))
+    def _constant[A](a: A): Stream[A] = unfold(a)(a => Some(a, a))
+    def _ones: Stream[Int] = unfold(())(_ => Some(1, ()))
+
   }
 }
--- src/test/scala/fp/exercise/C5Test.scala
+++ src/test/scala/fp/exercise/C5Test.scala
@@ -59,4 +59,112 @@ class C5Test extends FunSuite with DiagrammedAssertions {
     val fib = Stream.fibs.take(8)
     assert(fib.toList === List(0, 1, 1, 2, 3, 5, 8, 13))
   }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // unfold
+
+  test("_constant") {
+    val s = Stream._constant(2)
+    assert(s.take(2).toList === List(2, 2))
+    assert(s.take(1).toList === List(2))
+    assert(s.take(0).toList === List())
+  }
+
+  test("_from") {
+    val s = Stream._from(2)
+    assert(s.take(2).toList === List(2, 3))
+    assert(s.take(1).toList === List(2))
+    assert(s.take(0).toList === List())
+  }
+
+  test("_fibs") {
+    val fib = Stream._fibs.take(8)
+    assert(fib.toList === List(0, 1, 1, 2, 3, 5, 8, 13))
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  test("_map") {
+    val s = Stream(1, 2)._map(_.toString).toList
+    assert(s === List("1", "2"))
+  }
+
+  test("_take") {
+    val s = Stream(1, 2)
+    assert(s._take(1).toList === List(1))
+    assert(s._take(3).toList === List(1, 2))
+  }
+
+  test("_takeWhile") {
+    val s = Stream(1, 2, 3, 4)
+    val f1 = (i: Int) => i < 3
+    val f2 = (i: Int) => i > 5
+    assert(s._takeWhile(f1).toList === List(1, 2))
+    assert(s._takeWhile(f2).toList === List())
+  }
+
+  test("zipWith") {
+    val s = Stream(1, 2)
+    val s1 = Stream(1)
+    val s2 = Stream(4, 5, 6)
+
+    assert(s.zipWith(s1).toList === List((1, 1)))
+    assert(s.zipWith(s2).toList === List((1, 4), (2, 5)))
+  }
+
+  test("zipAll") {
+    val s = Stream(1, 2)
+    val s1 = Stream(1)
+
+    assert(s.zipAll(s1).toList === List((Some(1), Some(1)), (Some(2), None)))
+    assert(s1.zipAll(s).toList === List((Some(1), Some(1)), (None, Some(2))))
+  }
+
+  test("exists") {
+    val s = Stream(1, 2)
+    val f1 = (a: Int) => a == 1
+    val f2 = (a: Int) => a > 3
+
+    assert(s.exists(f1) === true)
+    assert(s.exists(f2) === false)
+  }
+
+  test("hasSubsequence") {
+    val s = Stream(1,2,3)
+    val s1 = Stream(1,3)
+    val s2 = Stream(2,3)
+
+    assert(s.hasSubsequence(s1) === false)
+    assert(s.hasSubsequence(s2) === true)
+    assert(s.hasSubsequence(s) === true)
+
+    val a = Stream.empty[Int]
+    assert(a.hasSubsequence(a) === true)
+  }
+
+  test("startWith") {
+    val s = Stream(1, 2, 3)
+    val s2 = Stream(1, 2)
+    val s3 = Stream(1, 3, 2)
+
+    assert(s.startsWith(s2) === true)
+    assert(s.startsWith(s3) === false)
+
+    assert(s._startsWith(s2) === true)
+    assert(s._startsWith(s3) === false)
+  }
+
+  test("tails") {
+    val s = Stream(1, 2, 3)
+    val ss = s.tails.map(_.toList).toList
+    assert(ss === List(List(1, 2, 3), List(2, 3), List(3), Nil))
+  }
+
+  test("scanRight") {
+    val l = Stream(1, 2, 3).scanRight(0)(_ + _).toList
+    assert(l === List(6, 5, 3, 0))
+
+    val l2 = Stream(1, 2, 3)._scanRight(0)(_ + _).toList
+    assert(l2 === List(6, 5, 3, 0))
+  }
 }
